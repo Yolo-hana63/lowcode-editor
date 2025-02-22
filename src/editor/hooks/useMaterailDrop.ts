12d3a98fcc8392017547fb509b6e3b53274c0990
Yolo-hana63 <553875647@qq.com>
2025-02-22 18:33:31 +0800

    feat: 物料区大纲及源码

--- src/editor/hooks/useMaterailDrop.ts
+++ src/editor/hooks/useMaterailDrop.ts
@@ -3,33 +3,34 @@ import { useComponentConfigStore } from "../stores/component-config";
 import { useComponetsStore } from "../stores/components";
 
 export function useMaterailDrop(accept: string[], id: number) {
-    const { addComponent } = useComponetsStore();
-    const { componentConfig } = useComponentConfigStore();
+  const { addComponent } = useComponetsStore();
+  const { componentConfig } = useComponentConfigStore();
 
-    const [{ canDrop }, drop] = useDrop(() => ({
-        accept,
-        drop: (item: { type: string}, monitor) => {
-            const didDrop = monitor.didDrop()
-            if (didDrop) {
-              return;
-            }
+  const [{ canDrop }, drop] = useDrop(() => ({
+    accept,
+    drop: (item: { type: string }, monitor) => {
+      const didDrop = monitor.didDrop();
+      if (didDrop) {
+        return;
+      }
 
-            const config = componentConfig[item.type]
+      const config = componentConfig[item.type];
 
-            addComponent({
-                id: new Date().getTime(),
-                name: item.type,
-                desc: config.desc,
-                props: config.defaultProps,
-                styles: {
-                  background: 'green'
-                }
-            }, id)
+      addComponent(
+        {
+          id: new Date().getTime(),
+          name: item.type,
+          desc: config.desc,
+          props: config.defaultProps,
+          styles: {},
         },
-        collect: (monitor) => ({
-          canDrop: monitor.canDrop(),
-        }),
-    }));
+        id
+      );
+    },
+    collect: (monitor) => ({
+      canDrop: monitor.canDrop(),
+    }),
+  }));
 
-    return { canDrop, drop }
+  return { canDrop, drop };
 }
